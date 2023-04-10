249f3d340ae9f8e2dd649278f34e2cc72736b838
Paruyr Khachatryan <dangerkhachatryan@gmail.com>
2023-04-10 18:22:13 +0400

    Added MovieRepository class

---
+++ src/main/java/aca/demo/movierating/movie/MovieRepository.java
@@ -0,0 +1,38 @@
+package aca.demo.movierating.movie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.HexFormat.of;
+@Component
+@Slf4j
+public class MovieRepository {
+    private List<Movie> movies = new ArrayList<>();
+
+    public Optional<Movie> findByTitle(String title) {
+        log.debug("method findByTitle - {}" +
+                "running");
+        List<Movie> movieList = movies.stream().distinct().filter(movie1 -> movie1.getTitle().equals(title)).collect(Collectors.toList());
+        Optional<Movie> optionalMovie = Optional.of(movieList.get(0));
+        return optionalMovie.isEmpty() ? Optional.empty() : optionalMovie;
+    }
+
+    public List<Movie> findByGenre(Genre genre) {
+        log.debug("method findByGenre - {}" +
+                "running");
+        return movies.stream().filter(movie -> movie.getGenre().equals(genre)).collect(Collectors.toList());
+    }
+
+    public void save(CreateMovie createMovie) {
+        log.debug("method save Movie - {}" +
+                "running");
+        Movie movie = new Movie(createMovie);
+        movies.add(movie);
+    }
+}

86eac666f775438c40296bbb883fbe2fbb7294c6
Paruyr Khachatryan <dangerkhachatryan@gmail.com>
2023-04-10 18:35:21 +0400

    Added MovieService class

---
+++ src/main/java/aca/demo/movierating/movie/MovieService.java
@@ -0,0 +1,23 @@
+package aca.demo.movierating.movie;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public List<Movie> search(Genre genre) {
+        return movieRepository.findByGenre(genre);
+    }
+
+    public void create(CreateMovie createMovie) {
+        //Exception check in the MoveRepository save method
+        movieRepository.save(createMovie);
+    }
+}

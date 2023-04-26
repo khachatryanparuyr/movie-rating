88a5a93e87d2f11c460306948e9539e2f8aa701e
Paruyr Khachatryan <dangerkhachatryan@gmail.com>
2023-04-26 14:10:20 +0400

    added all fields and constructing in the class Movie

--- src/main/java/aca/demo/movierating/movie/Movie.java
+++ src/main/java/aca/demo/movierating/movie/Movie.java
@@ -6,19 +6,33 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.LocalDate;
+
 @Getter
 @Slf4j
-@EqualsAndHashCode
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
 @Setter
 public class Movie {
+
+    @EqualsAndHashCode.Include
+    private Long id;
+
     private String title;
     private Genre genre;
+    private LocalDate releasedAt;
+    private String director;
+    private double rating;
+
 
     public Movie(CreateMovie createMovie) {
         this.title = createMovie.getTitle();
-        this.genre= createMovie.getGenre();
-        log.debug("Constructing movie with createMovie - {}" +
-                "represent the createMovie parameter");
+        this.genre = createMovie.getGenre();
+        this.director = createMovie.getDirector();
+        this.rating = createMovie.getRating();
+        this.releasedAt = createMovie.getReleasedAt();
+        log.debug("Constructing movie with createMovie - {}"
+                , this.title + " " + this.genre + " "
+                        + this.director + " " + this.rating + " " + this.releasedAt);
     }
 }
